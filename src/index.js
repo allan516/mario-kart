8216b5bf99c131da7a8bbbbb2d330e7b776fcbdc
Allan <allan.mendes0976@gmail.com>
2024-11-21 11:08:18 -0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -106,6 +106,33 @@ async function playRaceEngine(character1, character2) {
     if (block === "CONFRONTO") {
       let powerResult1 = diceResult1 + character1.PODER;
       let powerResult2 = diceResult2 + character2.PODER;
+
+      console.log(`${character1.NOME} confrontou com ${character2.NOME}! 🥊`);
+
+      await logRollResult(
+        character1.NOME,
+        "poder",
+        diceResult1,
+        character1.PODER
+      );
+
+      await logRollResult(
+        character2.NOME,
+        "poder",
+        diceResult2,
+        character2.PODER
+      );
+
+      character2.PONTOS -=
+        powerResult1 > powerResult2 && character2.PONTOS > 0 ? 1 : 0;
+
+      character1.PONTOS -= powerResult2 && character2.PONTOS > 0 ? 1 : 0;
+
+      console.log(
+        powerResult2 === powerResult1
+          ? "confronto empatado! Nenhum ponto foi perdido."
+          : ""
+      );
     }
 
     if (totalTestSkill1 > totalTestSkill2) {
