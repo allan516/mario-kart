4f0ab4249d0fc1e4ae9944425988c5c5961b60eb
Allan <allan.mendes0976@gmail.com>
2024-11-22 13:55:10 -0300

    feat: add new players

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+// characters
+
+const players = [
+  { NOME: "Mario", VELOCIDADE: 4, MANOBRABILIDADE: 3, PODER: 3, PONTOS: 0 },
+  { NOME: "Luigi", VELOCIDADE: 3, MANOBRABILIDADE: 4, PODER: 4, PONTOS: 0 },
+  { NOME: "Peach", VELOCIDADE: 3, MANOBRABILIDADE: 4, PODER: 2, PONTOS: 0 },
+  { NOME: "Yoshi", VELOCIDADE: 3, MANOBRABILIDADE: 4, PODER: 3, PONTOS: 0 },
+  {
+    NOME: "Donkey Kong",
+    VELOCIDADE: 2,
+    MANOBRABILIDADE: 2,
+    PODER: 5,
+    PONTOS: 0,
+  },
+  { NOME: "Bowser", VELOCIDADE: 5, MANOBRABILIDADE: 2, PODER: 5, PONTOS: 0 },
+];
+
+console.log(players[1].NOME);
+
 const player1 = {
   NOME: "Mario",
   VELOCIDADE: 4,
